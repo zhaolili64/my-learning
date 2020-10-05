6824bc288b5e04a853c06f4a0adcd8b801bc5c98
zhaolili64 <1535716054@qq.com>
2020-10-05 18:35:42 +0800

    Initial commit

---
+++ PractiseBase/src/com/zll/java/week/week1/Main.java
@@ -0,0 +1,13 @@
+package com.zll.java.week.week1;
+
+/**
+ * @ClassName Main
+ * @Description TODO
+ * @Author 86180
+ * @Date 2020/10/5
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
