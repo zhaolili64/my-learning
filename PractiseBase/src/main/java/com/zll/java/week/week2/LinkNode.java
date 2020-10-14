acbcf715d63169d99b01af81f744369c2ddd8cd7
zhaolili64 <1535716054@qq.com>
2020-10-14 19:20:56 +0800

    class

---
+++ PractiseBase/src/main/java/com/zll/java/week/week2/LinkNode.java
@@ -0,0 +1,20 @@
+package com.zll.java.week.week2;
+
+/**
+ * @ClassName LinkNode
+ * @Description TODO
+ * @Author 86180
+ * @Date 2020/10/13
+ **/
+    public class LinkNode {
+    char data;
+    LinkNode next;
+    public  LinkNode()
+    {
+        next = null;}
+        public LinkNode(char ch)
+        {
+            data=ch;
+            next = null;
+        }
+    }
