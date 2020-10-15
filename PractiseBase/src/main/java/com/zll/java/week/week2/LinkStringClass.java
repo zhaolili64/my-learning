7a9484e4ad78f1f1ee9b1ecdcea0cc1fd35d66e4
zhaolili64 <1535716054@qq.com>
2020-10-15 20:04:44 +0800

    class

---
+++ PractiseBase/src/main/java/com/zll/java/week/week2/LinkStringClass.java
@@ -0,0 +1,17 @@
+package com.zll.java.week.week2;
+
+/**
+ * @ClassName LinkStringClass
+ * @Description TODO
+ * @Author 86180
+ * @Date 2020/10/13
+ **/
+public class LinkStringClass {
+    LinkNode head;
+    int size;
+    public LinkStringClass()
+    {
+        head=new LinkNode();
+        size =0;
+    }
+}
