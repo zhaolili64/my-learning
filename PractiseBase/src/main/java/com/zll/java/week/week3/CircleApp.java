43d7050e4bd3230c97e86cbec35ab66b1abe161b
zhaolili64 <1535716054@qq.com>
2020-10-23 21:34:40 +0800

    java面向程序

---
+++ PractiseBase/src/main/java/com/zll/java/week/week3/CircleApp.java
@@ -0,0 +1,14 @@
+package com.zll.java.week.week3;
+
+/**
+ * @ClassName CircleApp
+ * @Description TODO
+ * @Author 86180
+ * @Date 2020/10/19
+ **/
+public class CircleApp {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(10,6,8,6);
+        cylinder.print();
+    }
+}
