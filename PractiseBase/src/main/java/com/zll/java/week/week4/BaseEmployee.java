3f5da00d6ad25e97e834ddcf3c6cac8337b06ae2
zhaolili64 <1535716054@qq.com>
2020-10-26 15:09:43 +0800

    java面向程序-employee

---
+++ PractiseBase/src/main/java/com/zll/java/week/week4/BaseEmployee.java
@@ -0,0 +1,15 @@
+package com.zll.java.week.week4;
+
+/**
+ * @ClassName biubiu
+ * @Description TODO
+ * @Author 86180
+ * @Date 2020/10/26
+ **/
+public abstract class BaseEmployee {
+    public  void  print(){
+        System.out.println("This is an abstract calss.");
+    }
+
+    public  abstract  int getSalary( );
+}
---
+++ PractiseBase/src/main/java/com/zll/java/week/week4/OtherEmployee.java
@@ -0,0 +1,25 @@
+package com.zll.java.week.week4;
+
+/**
+ * @ClassName OtherEmployee
+ * @Description TODO
+ * @Author 86180
+ * @Date 2020/10/26
+ **/
+public class OtherEmployee extends BaseEmployee{
+    private  String gender;
+
+    public  OtherEmployee(String gender){
+        this.gender = gender;
+    }
+
+    public OtherEmployee(){
+
+    }
+
+
+    @Override
+    public int getSalary() {
+        return 0;
+    }
+}
